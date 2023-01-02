3ae653d240d7021590d208dd59d382f9b0e3eb55
Adeoye John Oluwakayode <100791994+John-Fixit@users.noreply.github.com>
2023-01-02 02:17:57 +0100

    Delete Server directory
    Client and server side is been separated

--- Server/Routes/messagesRoute.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const messageController = require('../Controller/messageController')
-const messageRouter = express.Router()
-
-messageRouter.post('/addMsg', messageController.addMessage)
-messageRouter.post('/getMsg', messageController.getallMessage)
-module.exports = messageRouter
